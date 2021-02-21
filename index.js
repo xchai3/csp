f954852ebaec2db749b04facf46d0030776ba33f
xchai3 <xipengc@andrew.cmu.edu>
2021-02-20 18:29:09 -0800

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 5000;
+
+let sendEvil = (req, res) => {
+    res.send("<script>alert(`Hello`)</script>");
+}
+
+app.get('/', sendEvil)
+
+// app.get('/evil.html', sendEvil)
+
+app.listen(port, () => {
+    console.log(`evil.local server listening on port ${port}!`)
+})
